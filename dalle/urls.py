fd7729c85528b984fe459cb883045cb69832ab97
zero <wmk1024@gmail.com>
2022-11-02 13:44:10 +0900

    add setting page

--- dalle/urls.py
+++ dalle/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('setting/', views.setting, name='setting'),
+
 ]
 
 #urlpatterns += staticfiles_urlpatterns()
