6eead125530547a77f1d76f37591517ec7fac916
hanna <artemiss02@hanmail.net>
2022-11-03 17:35:28 +0900

    통합본 pull 오류 해결중

--- salon/views.py
+++ salon/views.py
@@ -1,25 +1,33 @@
+from django.shortcuts import render
 import json
 from django.http import JsonResponse
-from django.shortcuts import render
-# from .dalle import dalle
-from . import music
-from mypage.models import Member
+from django.contrib import auth
 from salon.models import ImageUploadModel, MusicUploadModel, KeywordModel
-# import MinDalle
-# model = MinDalle(is_mega=True, is_reusable=True)
+import os
+import openai
+from PIL import Image
+import requests
+from io import BytesIO
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 import time
 
 
 def index(request):
     return render(request, 'salon/index.html', {})
 
+def main(request):
+    return render(request, 'salon/main.html')
+
 
 def home(request):
-    keywords = ['가장 재미있는','추천이 많은', 'Best 작품', '회원님이 좋아할만한 작품', "Today's Favorite"]   
-    return render(request, 'salon/home.html', {'keywords':keywords})
+    keywords = ['가장 재미있는','추천이 많은', 'Best 작품', '회원님이 좋아할만한 작품', "Today's Favorite"]
+    image = ImageUploadModel.objects.get(name='melon_image')
+    return render(request, 'salon/home.html', {'keywords':keywords, 'image':image})
 
 def search(request):
     if request.method == 'POST':
@@ -36,75 +44,106 @@ def start(request):
 
 # 출력창
 def result(request):
-    text = ''
-    if request.method == "POST":
-        text = request.POST['title']
-        # music_file = music.generateMusic()
-        music_file = 'MuseGAN'
-    # 이미지 파일이 나온다.
-    # img = model.generate_image(text, 7, 1) 이곳에 모델 연결
-    img = 'img'
-    img_file =  text + '.png'
-    # img = Image.open('./media/a.png')
-    # img.save('./media/' + img_file, 'png')
+    text, image_url = image_generation(request)
+    
+    # music_file = '/media/musics/' + music.generateMusic()
+    # image_url = 'https://ifh.cc/g/5qCAX2.jpg'
+    music_file = '/media/musics/MuseNet-Composition.mid'
+
+    # 이미지 & 섬네일 media에 저장
+    res = requests.get(image_url)
+    img_file = Image.open(BytesIO(res.content))
+    img_file.save('media/images/'+text+'.jpg')
+    img_file.thumbnail((300, 300))
+    img_file.save('media/images/'+text+'_tn.jpg')
+
 
     # 텍스트 -> 태그화 리스트
     only_english = re.sub('[^a-zA-Z]', ' ', text)   # 영어만 남기기
-    no_capitals = only_english.lower().split()      # 대문자 -> 소
-    stops = set(stopwords.words('english'))         # 불용어 제거
-    no_stops = [word for word in no_capitals if not word in stops]
+    only_english_lower = only_english.lower()       # 대문자 -> 소
+    word_tokens =  nltk.word_tokenize(only_english_lower)   # 토큰화
+    tokens_pos = nltk.pos_tag(word_tokens)          # 품사 분류
+    
+    # 명사만 뽑기
+    NN_words = [word for word, pos in tokens_pos if 'NN' in pos]
 
-    stemmer = nltk.stem.SnowballStemmer('english')  # 어간 추출
-    tags = [stemmer.stem(word) for word in no_stops]
+    # 원형 추출
+    wlem = WordNetLemmatizer()
+    lemmatized_words = []
+    for word in NN_words:
+        new_word = wlem.lemmatize(word)
+        lemmatized_words.append(new_word)
 
-
-    # 유저 정보와 같이 저장 필요
-    for tag in tags:
-        try:
-            exist_word = KeywordModel.objects.get(word=tag)
-            exist_word.input_num += 1
-            exist_word.save()
-        except:
-            word = KeywordModel(word=tag)
-            word.input_num += 1
-            word.save()
-
+    # 불용어 제거 - stopwords_list 에 따로 추가 가능
+    stopwords_list = set(stopwords.words('english'))
+    no_stops = [word for word in lemmatized_words if not word in stopwords_list]
+    
     context = {'text': text, 
-                'img':img, 
+                'img_file':'/media/images/'+text+'.jpg', 
                 "music_file":music_file, 
-                "img_file":img_file,
-                "tags":tags}
+                # 'img_url':image_url,
+                'tn_img':'/media/images/'+text+'_tn.jpg',
+                "tags":no_stops,
+    }
+
+    request.session['test_keyword'] = context
 
     return render(request, 'salon/result.html', context)
 
+def image_generation(request):
+    openai.organization = "org-IHDNUM52y3No3XxvBFRpbIf5"
+    openai.api_key = "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"
+
+    text = ''
+    if request.method == "POST":
+        text = request.POST['title']
+        response = openai.Image.create( prompt=text,
+                                n=1,
+                                size="1024x1024")
+        image_url = response['data'][0]['url']
+    return text,image_url
+
 
 def save_result(request):
+    context = request.session['test_keyword']
+    keywords = context['tags']
+    for keyword in keywords:
+        try:
+            exist_word = KeywordModel.objects.get(word=keyword)
+            exist_word.input_num += 1
+            exist_word.save()
+        except:
+            word = KeywordModel(word=keyword)
+            word.input_num += 1
+            word.save()
     if request.method == 'POST':
-        # member_id = request.session.get('user') # request.POST.get("member_id")
-        # member_id = request.session['user'] # dict로 id 키값을 가져옴
         user = request.user
         selected = request.POST.getlist("selected")
+        text = request.POST.get("input_text")
+ 
         for filepath in selected:
+            # 유저 정보와 같이 저장 필요
             if 'mid' == filepath[-3:]:
-                musicfile = MusicUploadModel(user=user, name="music", filename=filepath)
-                musicfile.save()
+                musicfile = MusicUploadModel(user=user, name=text+"_music", filename=filepath, input_text=text)
+                # musicfile.save()
             else:
-                imgfile = ImageUploadModel(user=user, name="photo", filename=filepath)
+                filename = filepath.split(' ')[0]
+                thumbnail = filepath.split(' ')[1]
+                imgfile = ImageUploadModel(user=user, name=text+"_image", filename=filename, thumbnail=thumbnail, input_text=text)
                 imgfile.save()
         return render(request, 'salon/save_result.html', {'files':selected})
     
     return render(request, 'salon/save_result.html', {})
 
+@csrf_exempt
 def result_favorite(request):
     if request.method == 'POST':
-        # user_id = request.session.get('user')
-        # user = Member.objects.get(id=user_id)
         user = request.user
         json_data = json.loads( request.body )
         favorite = json_data['aa'] # favorite = str( json_data['aa'] ) or # models.py: CharField -> IntegerField 
     
 
-        print("=============================", user.username, favorite)
+        print("=============================", user.username, json_data)
 
 
         # 데이터타입 체크 # if type(favorite) is int
@@ -114,16 +153,16 @@ def result_favorite(request):
             print("result_code's dtype: ", type(fv))
             for favorite in fv:
                 musicfile = MusicUploadModel(user=user, result_favorite=favorite)
-                musicfile.save()
+                # musicfile.save()
                 imgfile = ImageUploadModel(user=user, result_favorite=favorite)
-                imgfile.save()
+                # imgfile.save()
         else:
             print("result_code's dtype: ", type(favorite))
             for favorite in favorite:
                 musicfile = MusicUploadModel(user=user, result_favorite=favorite)
-                musicfile.save()
+                # musicfile.save()
                 imgfile = ImageUploadModel(user=user, result_favorite=favorite)
-                imgfile.save()
+                # imgfile.save()
 
         # 데이터타입 체크 if문이 없을 때 사용
         # for favorite in favorite:
@@ -138,7 +177,6 @@ def result_favorite(request):
         data = {'result':'kwang'}
         return JsonResponse(data)
 
-# 모델 호출 함수
 def result_model(request):
     json_data = json.loads( request.body )
     text = json_data['aa']
@@ -148,4 +186,3 @@ def result_model(request):
     img_file = 'aa.png'
     data = {'result':'successful', 'result_code': '1', 'imgfile':img_file, 'musfile':music_file}
     return JsonResponse(data)
-
