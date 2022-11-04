12b71829825c4820aa90e9b09bf185734762d54c
jiwon <gone1011@naver.com>
2022-11-04 15:56:52 +0900

    마음에 드시나요 버튼 반영

--- salon/admin.py
+++ salon/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ImageKeywordModel, KeywordModel, ImageUploadModel, MusicKeywordModel, MusicUploadModel, Member
+from .models import ImageKeywordModel, KeywordModel, ImageUploadModel, MusicKeywordModel, MusicUploadModel
+from mypage.models import Member
 
 # Register your models here.
 admin.site.register(Member)
