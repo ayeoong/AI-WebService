50a9b9b05501c5050933603f203e5f4b81116c68
hanna <artemiss02@hanmail.net>
2022-11-08 16:16:51 +0900

    회원가입 폼 유효성 검사 추가

--- mypage/forms.py
+++ mypage/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserForm(forms.ModelForm):
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일")
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'password1', 'password2', 'email']
 
 class LoginForm(forms.ModelForm):
     class Meta:
