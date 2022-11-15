187441440bddf6c35063f93911938c704104a5c0
jiwon <gone1011@naver.com>
2022-11-15 14:36:59 +0900

    #161 result_favorite 진행하지 않고 바로 저장하기 때문에 불필요한 코드 삭제

--- salon/urls.py
+++ salon/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('result/', views.result, name='result'),
     path('save_result/', views.save_result, name='save_result'),
     path('search/', views.search, name='search'),
-    path('result_favorite/', views.result_favorite, name='result_favorite'),
     path('result_model/', views.result_model, name='result_model')
 ]
 
