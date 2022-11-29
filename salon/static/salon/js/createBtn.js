adeef999e60a432ec1db68a7abccc5e8c9431a3c
jiwon <gone1011@naver.com>
2022-11-29 17:21:27 +0900

    #210 완료

--- salon/static/salon/js/createBtn.js
+++ salon/static/salon/js/createBtn.js
@@ -4,8 +4,7 @@ function activateBtn() {
   const btn = document.querySelector('#btn_create'),
   title = document.querySelector('#title').value,
   checktext = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|\s]*$/; // 한글숫자영어만(공백포함) 입력가능
-
-  btn.disabled = !(checktext.test(title) && (title.length != 0));
+  btn.disabled = !(checktext.test(title) && (title.length>4));
 
 }
 inputBox.addEventListener('keyup', activateBtn);
