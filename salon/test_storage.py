4b8310bbc09fe60a3f5b76ae8dceae91a7313d45
moon0825 <66240957+moon0825@users.noreply.github.com>
2022-11-30 16:04:34 +0900

    파일생성 및 삭제 salon utils에 빼기 전 저장

--- salon/test_storage.py
+++ salon/test_storage.py
@@ -181,3 +181,16 @@ class StorageTestClass(TestCase):
         blob.delete()
 
         print(f"Blob {mus_filename} deleted.")
+
+        def delete_blob(bucket_name, blob_name):
+            """Deletes a blob from the bucket."""
+            # bucket_name = "your-bucket-name"
+            # blob_name = "your-object-name"
+
+            storage_client = storage.Client()
+
+            bucket = storage_client.bucket(bucket_name)
+            blob = bucket.blob(blob_name)
+            blob.delete()
+
+            print(f"Blob {blob_name} deleted.")        
