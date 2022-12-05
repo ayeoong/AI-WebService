c3fac815223f9a5eeed9614b45237a1978cce860
hanna <artemiss02@hanmail.net>
2022-12-05 19:29:22 +0900

    비밀번호 재설정 페이지 커스터마이징

--- dalle/urls.py
+++ dalle/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
     path('find_id/', views.find_id, name='find_id'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('setting/', views.setting, name='setting'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
+	path('password_reset_done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
